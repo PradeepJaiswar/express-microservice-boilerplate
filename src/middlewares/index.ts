b3f71a4b35cceabfe86676314ae9f07e71a006f3
Pradeep Jaiswar <mado023@gmail.com>
2020-07-01 12:31:29 +0530

    Remove express-http-context

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,9 +1,7 @@
 import defaultMiddleware from "./default";
-import uuidMiddleware from "./uuid";
 import expressWinston from "./express-winston";
 
 export default  {
     defaultMiddleware,
-    uuidMiddleware,
     expressWinston,
 };
